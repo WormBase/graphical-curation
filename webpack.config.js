145d4615b631b922ee4e549e04fa05238c0b9abd
Valerio Arnaboldi <valearna@caltech.edu>
2020-04-27 11:46:22 -0700

    feat: export single components

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,15 @@ var path = require('path');
 
 module.exports = {
     mode: 'production',
-    entry: './src/App.js',
+    entry: {
+        'main.js': './src/App.js',
+        'expression/annotator.js': './src/components/ExpressionAnnotator.js',
+        'expression/viewer.js': './src/components/ExpressionAnnotationsViewer.js',
+        'entity-picker.js': './src/components/EntityPicker.js'
+    },
     output: {
         path: path.resolve('lib'),
-        filename: 'App.js',
+        filename: '[name].js',
         libraryTarget: 'commonjs2'
     },
     module: {
