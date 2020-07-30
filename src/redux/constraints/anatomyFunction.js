7bdca09fbd3f42c7b0bc227aea3212afdf573a1a
Valerio Arnaboldi <valearna@caltech.edu>
2020-07-30 11:15:27 -0700

    !fix(anatomyFunction): changed annotation definition
    enabling annotations without genes

--- src/redux/constraints/anatomyFunction.js
+++ src/redux/constraints/anatomyFunction.js
@@ -1,11 +1,9 @@
 export const anatomyFunctionAnnotationHasPhenotype = annotation => annotation.phenotype !== undefined && annotation.phenotype !== '';
 
-export const anatomyFunctionAnnotationHasGenes = annotation => annotation.genes.length > 0;
-
 export const anatomyFunctionAnnotationHasAnatomyTerms = annotation => annotation.anatomyTerms.length > 0
 
 export const anatomyFunctionAnnotationHasAssay = annotation => annotation.assay !== '';
 
 export const anatomyFunctionAnnotationIsValid = annotation => anatomyFunctionAnnotationHasPhenotype(annotation) &&
-    anatomyFunctionAnnotationHasGenes(annotation) && anatomyFunctionAnnotationHasAnatomyTerms(annotation) &&
-    annotation.evidence !== undefined && anatomyFunctionAnnotationHasAssay(annotation)
+    anatomyFunctionAnnotationHasAnatomyTerms(annotation) && annotation.evidence !== undefined &&
+    anatomyFunctionAnnotationHasAssay(annotation)
